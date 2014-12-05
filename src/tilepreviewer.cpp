06ff2d0748d9aa88516ed7b27057d271d7f7e554
Fredrik S <fredde.sander@gmail.com>
2014-12-05 15:43:02 +0100

    Preview tiles now stay in a position relative to the viewport

--- src/tilepreviewer.cpp
+++ src/tilepreviewer.cpp
@@ -1,21 +1,39 @@
 #include "tilepreviewer.hpp"
 
-#include <iostream>
+const sf::Vector2f TilePreviewer::previewSize(30, 30);
 
 TilePreviewer::TilePreviewer()
 {
     mShape.setSize(sf::Vector2f(40, 800));
     mShape.setFillColor(sf::Color(46, 46, 44));
+
+    border.x = 5;
+    border.y = 175;
+
+    rectSpace.y = 50;
+
+    for (int i = 0; i < 9; i++)
+    {
+        sf::RectangleShape shape(previewSize);
+        shape.setPosition(border.x, border.y + rectSpace.y * i);
+        rectPreviewers.push_back(shape);
+    }
 }
 
 void TilePreviewer::update(sf::Vector2f &relPos)
 {
     mShape.setPosition(relPos);
+
+    for (int i = 0; i < 9; i++)
+        rectPreviewers.at(i).setPosition(border.x + relPos.x, relPos.y + border.y + rectSpace.y * i);
 }
 
 void TilePreviewer::draw(sf::RenderTarget &target)
 {
     target.draw(mShape);
+
+    for (int i = 0; i < rectPreviewers.size(); i++)
+        target.draw(rectPreviewers.at(i));
 }
 
 void TilePreviewer::setSelector(Selector *nSelector)
--- src/tilepreviewer.hpp
+++ src/tilepreviewer.hpp
@@ -2,6 +2,7 @@
 #define TILEPREVIEWER_HPP
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include "selector.hpp"
 
 class TilePreviewer
@@ -12,8 +13,12 @@ class TilePreviewer
         void draw(sf::RenderTarget &target);
         void setSelector(Selector *nSelector);
     private:
+        static const sf::Vector2f previewSize;
+        sf::Vector2f border, rectSpace;
         sf::RectangleShape mShape;
         Selector *selector;
+
+        std::vector<sf::RectangleShape> rectPreviewers;
 };
 
 #endif
